517514866f4566e0762ee634b5535b864f78f9f5
PApi0987 <msigma997@gmail.com>
2026-04-03 22:35:23 +0100

    Refactor Firebase configuration and imports
    Removed Firestore import and configuration, updated comments for clarity.

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,19 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your Firebase config
+// YOUR CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyBZu6gLeNwi0AcrZlmVdGWxjiNW9gPRb1U",
   authDomain: "plexexamhelp.firebaseapp.com",
-  databaseURL: "https://plexexamhelp-default-rtdb.firebaseio.com",
   projectId: "plexexamhelp",
   storageBucket: "plexexamhelp.firebasestorage.app",
   messagingSenderId: "707616782385",
   appId: "1:707616782385:web:93aecbcc1ec6dace923d10"
 };
 
-// Initialize Firebase
+// INIT
 const app = initializeApp(firebaseConfig);
 
-// EXPORT SERVICES (THIS IS IMPORTANT)
+// EXPORT AUTH
 export const auth = getAuth(app);
-export const db = getFirestore(app);
