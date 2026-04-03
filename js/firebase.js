85f61406ee45e9706cadbc2280743219fe2e0272
PApi0987 <msigma997@gmail.com>
2026-04-04 00:02:19 +0100

    Add Firestore initialization to firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,13 @@
-// firebase.js
+<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// YOUR CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyBZu6gLeNwi0AcrZlmVdGWxjiNW9gPRb1U",
   authDomain: "plexexamhelp.firebaseapp.com",
   projectId: "plexexamhelp",
-  storageBucket: "plexexamhelp.firebasestorage.app",
+  storageBucket: "plexexamhelp.appspot.com",
   messagingSenderId: "707616782385",
   appId: "1:707616782385:web:93aecbcc1ec6dace923d10"
 };
@@ -15,5 +15,7 @@ const firebaseConfig = {
 // INIT
 const app = initializeApp(firebaseConfig);
 
-// EXPORT AUTH
+// SERVICES
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+</script>
